596f13a06baba52b79fe16f8896fa7e232b0529c
Sviatlana Klimenkova <s.klimenkova@sam-solutions.com>
2019-04-16 21:31:57 +0300

    Initial commit

---
+++ src/main/java/by/svkli/fitness/cooker/food_server/FoodServerApplication.java
@@ -0,0 +1,13 @@
+package by.svkli.fitness.cooker.food_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FoodServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/by/svkli/fitness/cooker/food_server/FoodServerApplicationTests.java
@@ -0,0 +1,16 @@
+package by.svkli.fitness.cooker.food_server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodServerApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
